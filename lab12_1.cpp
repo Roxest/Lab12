39cf13401ec376a563a238a16bb6c9dd1bc482fb
Roxest <thammarin82@gmail.com>
2021-01-19 17:45:15 +0700

    FINISHED

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,37 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[] ,int N,double B[]){
+    double sum = 0;
+    for(int i=0;i<N;i++){
+        sum += A[i];
+    }
+    B[0] = sum/N;
+
+    double sumpow = 0;
+    for (int i = 0; i < N; i++){
+    sumpow+=pow(A[i]-B[0],2);
+    }
+    B[1]=pow(sumpow/(N),0.5);
+
+    double sumgeo = 1;   
+    for(int i=0;i<N;i++){
+        sumgeo *= A[i];
+    }
+    B[2] = pow(sumgeo,1/double(N));
+
+    double sumhar = 0;
+    for(int i=0;i<N;i++){
+        sumhar += 1/A[i];
+    }
+    B[3] = N/sumhar;
+    
+    double max = A[0], min =A[0];
+    for(int i=1;i<N;i++){
+        if(A[i]>max) max = A[i];
+        if(A[i]<min) min = A[i];
+    }
+    B[4] = max;
+    B[5] = min;    
+}
